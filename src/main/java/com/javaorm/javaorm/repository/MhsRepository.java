03935323163780a0d046e30fffe358e9ae96c21e
RicoBasyar <RicoBasyar@users.noreply.github.com>
2024-03-16 03:33:00 +0700

    Add Directory

---
+++ src/main/java/com/javaorm/javaorm/repository/MhsRepository.java
@@ -0,0 +1,8 @@
+package com.javaorm.javaorm.repository;
+
+import com.javaorm.javaorm.entity.MhsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MhsRepository extends JpaRepository<MhsEntity, Integer> {
+
+}
