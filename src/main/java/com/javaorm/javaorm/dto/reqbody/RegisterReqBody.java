b797c8276ec2cf0760e68bde8560635d13903d35
RicoBasyar <RicoBasyar@users.noreply.github.com>
2024-03-16 03:21:41 +0700

    Add project

---
+++ src/main/java/com/javaorm/javaorm/dto/reqbody/RegisterReqBody.java
@@ -0,0 +1,15 @@
+package com.javaorm.javaorm.dto.reqbody;
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterReqBody {
+
+}
