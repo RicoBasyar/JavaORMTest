a93f4611bb16a239fe62f29dd3fd738a810738dd
RicoBasyar <RicoBasyar@users.noreply.github.com>
2024-03-19 21:11:48 +0700

    Add Employe Create and Read Data

--- src/main/java/com/javaorm/javaorm/controller/EmployeController.java
+++ src/main/java/com/javaorm/javaorm/controller/EmployeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+// Test
 @RestController
 public class EmployeController {
     @Autowired
