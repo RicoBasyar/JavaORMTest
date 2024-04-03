01a5f920b6246879e42d57450fc9ef1fdb5a5ae4
Rico Basyar <47859890+RicoBasyar@users.noreply.github.com>
2024-04-03 10:48:15 +0700

    Test123

--- src/main/java/com/javaorm/javaorm/controller/MhsController.java
+++ src/main/java/com/javaorm/javaorm/controller/MhsController.java
@@ -10,8 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
-
 import java.util.logging.Logger;
+
 //
 @RestController
 public class MhsController {
